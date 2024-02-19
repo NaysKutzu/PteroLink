f875c9d39ebee241ad016d8123a1f60cf1e0df39
PteroLink CI <ci@pterolink.systems>
2024-02-19 17:55:18 +0000

    ci(release): bump version

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => 'canary',
+    'version' => '0.0.0',
 
     /*
     |--------------------------------------------------------------------------
